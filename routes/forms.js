4cc5f28eae8d08f134ccbf57d7f585a5476cc9ef
Daniel Esteban Novoa Reyes <dnovoare@everis.com>
2019-09-23 22:51:06 -0500

    Adds second and third form excercise

--- routes/forms.js
+++ routes/forms.js
@@ -7,14 +7,55 @@ var upload = multer({storage: storage, limits: {fileSize: 10000000}});
 
 var validator = require('../src/validator/form-lab-validator');
 
-/* GET home page. */
 router.get('/course', function(req, res, next) {
-    var data = req.params;
-    if (validator.validaCourseRequest(data)) {
-        res.render('course', data);
+  var data = req.query;
+  if (validator.validaCourseRequest(data)) {
+    res.render('course', data);
+  } else {
+    res.status(501).send('Error en validación de datos');
+  }
+});
+
+router.post('/subscribe', function(req, res, next) {
+  var data = req.body;
+  if (validator.validaSubscribeRequest(data)) {
+    if (data.password !== data.password_confirmation) {
+      res.status(501).send('Las contraseñas no coinciden');
     } else {
-        res.status(501).send('Error en validación de datos');
+      var users = req.cookies.users;
+      if (!users) {
+        users = [];
+      }
+      users.push(data);
+      res.cookie('users', users);
+      res.render('subscription', data);
     }
+  } else {
+    res.status(501).send('Error en validación de datos');
+  }
+});
+
+router.post('/login', function(req, res, next) {
+  var data = req.body;
+  if (validator.validaLoginRequest(data)) {
+    var users = req.cookies.users;
+    if (users) {
+      var currentUserData = null;
+      for (let user of users) {
+        if (user.username === data.username && user.password === data.password) {
+          currentUserData = user;
+          break;
+        }
+      }
+      if (currentUserData) {
+        res.render('login-success', currentUserData);
+      } else {
+        res.render('login-unsuccess', data);
+      }
+    }
+  } else {
+    res.status(501).send('Error en validación de datos');
+  }
 });
 
 router.post('/cv', upload.single('cv_file'), function(req, res, next) {
--- src/validator/form-lab-validator.js
+++ src/validator/form-lab-validator.js
@@ -10,6 +10,34 @@ var form_lab_validator = {
         isValid = isValid && !!body.identification_number;
         isValid = isValid && !!body.cv_file;
         return isValid;
+    },
+    validaCourseRequest: function(body) {
+        var isValid = true;
+        isValid = isValid && !!body.name;
+        isValid = isValid && !!body.identification;
+        isValid = isValid && !!body.birthdate;
+        isValid = isValid && !!body.gender;
+        isValid = isValid && !!body.ocupation;
+        isValid = isValid && !!body.hours_per_week;
+        isValid = isValid && !!body.provider;
+        return isValid;
+    },
+    validaSubscribeRequest: function(body) {
+        var isValid = true;
+        isValid = isValid && !!body.name;
+        isValid = isValid && !!body.surname;
+        isValid = isValid && !!body.provider;
+        isValid = isValid && !!body.username;
+        isValid = isValid && !!body.password;
+        isValid = isValid && !!body.password_confirmation;
+        return isValid;
+    },
+    validaLoginRequest: function(body) {
+        var isValid = true;
+        isValid = isValid && !!body.username;
+        isValid = isValid && !!body.password;
+        isValid = isValid && !!body.provider;
+        return isValid;
     }
 };
 
